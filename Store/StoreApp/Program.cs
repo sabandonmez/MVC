2ae66895f64c2d5adf9343580f9b53639143674d
Şaban Dönmez <eren.donmez.11@gmail.com>
2023-07-14 19:14:31 +0300

    Layout and Navbar for Area

--- Store/StoreApp/Program.cs
+++ Store/StoreApp/Program.cs
@@ -1,3 +1,5 @@
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
 using Repositories.Contracts;
@@ -7,14 +9,14 @@ using Services.Contracts;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddScoped<IRepositoryManager , RepositoryManager>();
-builder.Services.AddScoped<IProductRepository , ProductRepository>();
-builder.Services.AddScoped<ICategoryRepository , CategoryRepository>();
-builder.Services.AddScoped<IServiceManager , ServiceManager>();
-builder.Services.AddScoped<IProductService , ProductManager>();
-builder.Services.AddScoped<ICategoryService , CategoryManager>();
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
+builder.Services.AddScoped<IProductService, ProductManager>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
 
-builder.Services.AddDbContext<RepositoryContext>(options=>
+builder.Services.AddDbContext<RepositoryContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
      b => b.MigrationsAssembly("StoreApp"));
@@ -25,5 +27,12 @@ var app = builder.Build();
 app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseRouting();
-app.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    
+    endpoints.MapAreaControllerRoute("AdminRoute","Admin","admin/{controller=Dashboard}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+ 
+});
+
 app.Run();

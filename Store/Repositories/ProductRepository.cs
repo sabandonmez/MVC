6750760097f4991cb927e84e7435ce009fb858e5
Şaban Dönmez <eren.donmez.11@gmail.com>
2023-08-13 14:37:23 +0300

    Eager Loading & Repository Extension Enhancements

--- Store/Repositories/ProductRepository.cs
+++ Store/Repositories/ProductRepository.cs
@@ -1,9 +1,12 @@
 using Entities.Models;
+using Entities.RequestParameters;
+using Microsoft.EntityFrameworkCore;
 using Repositories.Contracts;
+using Repositories.Extensions;
 
 namespace Repositories
 {
-    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    public sealed class ProductRepository : RepositoryBase<Product>, IProductRepository
     {
         public ProductRepository(RepositoryContext repositoryContext) : base(repositoryContext)
         {
@@ -16,6 +19,11 @@ namespace Repositories
         
 
         public IQueryable<Product> GetAllProducts(bool trackChanges)=> FindAll(trackChanges);
+
+        public IQueryable<Product> GetAllProductsWithDetails(ProductRequestParameters p)
+        {
+            return _repositoryContext.Products.FilteredByCategoryId(p.CategoryId).FilteredBySearchTerm(p.SearchTerm).FilteredByPrice(p.MinPrice,p.MaxPrice,p.IsValidPrice);
+        }
 
         public Product? GetOneProduct(int id , bool trackChanges)
         {
